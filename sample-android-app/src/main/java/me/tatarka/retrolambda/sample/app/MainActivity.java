b170c26e7d16c9c4c9019a446a30e6b11ddef2f9
Evan Tatarka <evan@tatarka.me>
2015-08-10 21:00:28 -0400

    added databinding to the sample projects

--- sample-android-app/src/main/java/me/tatarka/retrolambda/sample/app/MainActivity.java
+++ sample-android-app/src/main/java/me/tatarka/retrolambda/sample/app/MainActivity.java
@@ -1,11 +1,14 @@
 package me.tatarka.retrolambda.sample.app;
 
-import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import javax.inject.Inject;
+
+import me.tatarka.retrolambda.sample.app.databinding.ActivityMainBinding;
+import me.tatarka.retrolambda.sample.lib.Function;
 
 /**
  * Created by evan on 3/29/15.
@@ -26,14 +29,18 @@ public class MainActivity extends AppCompatActivity {
                 .build()
                 .inject(this);
 
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         TextView text = (TextView) findViewById(R.id.text);
         text.setText(hello.run(getResources()));
 
         TextView textLib = (TextView) findViewById(R.id.text_lib);
         textLib.setText(libHello.run());
-        
+
+        Function helloDatabinding = () -> "Hello, Retrolambda! (from databinding)";
+        binding.setFun(helloDatabinding);
+        binding.executePendingBindings();
+
         ResFunction lambda = (res) -> "Foo";
     }
 }
